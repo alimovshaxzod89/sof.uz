a2e3f77b43d93c2ad26fb8a58b2c5ca70e625f3e
Shavkat Khamidjonov <homidjonov@gmail.com>
2019-09-24 20:38:39 +0500

    Alerts

--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -41,7 +41,7 @@ class ContactForm extends Model
             [['body'], 'string', 'max' => 24096],
             // email has to be a valid email address
             ['email', 'email'],
-            [['verifyCode'], YII_DEBUG ? 'safe' : ReCaptchaValidator::className(), 'message' => __('Бот емасмисиз?')],
+            [['verifyCode'], YII_DEBUG ? 'safe' : ReCaptchaValidator::className(), 'uncheckedMessage' => __('Бот емасмисиз?')],
         ];
     }
 
