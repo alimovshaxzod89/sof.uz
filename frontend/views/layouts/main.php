a7fe3f281d2177cd621e33a6b353d799ba281718
Shavkat Khamidjonov <shavkat.khamidjonov@activemedia.uz>
2020-04-23 09:07:52 +0500

    ad changes

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -223,10 +223,28 @@ $main_menu = Category::getCategoryTree([], Config::getRootCatalog());
                             e.preventDefault();
                             a('#banner-yandex').slideUp();
                         });
-                        a('#banner-yandex').initBanner({
-                            "place": "footer_fixed",
-                            "language": globalVars.l
-                        });
+                        (function (w, d, n, s, t) {
+                            w[n] = w[n] || [];
+                            w[n].push(function () {
+                                Ya.Context.AdvManager.render({
+                                        blockId: "R-A-476227-1",
+                                        renderTo: "yandex_rtb_R-A-476227-1",
+                                        async: true
+                                    },
+                                    function () {
+                                        jQuery('#banner-yandex').initBanner({
+                                            "place": "yandex-callback",
+                                            "language": globalVars.l
+                                        });
+                                    });
+                            });
+                            t = d.getElementsByTagName("script")[0];
+                            s = d.createElement("script");
+                            s.type = "text/javascript";
+                            s.src = "//an.yandex.ru/system/context.js";
+                            s.async = true;
+                            t.parentNode.insertBefore(s, t);
+                        })(window, document, "yandexContextAsyncCallbacks");
                     }
                 })
             }(jQuery);
