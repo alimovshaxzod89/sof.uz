86ac31f07d1cdd02c76360af6fddf7100dab70be
Muhammad Samadov <sam.muhammadali@gmail.com>
2022-05-16 14:15:19 +0500

    some code

---
+++ frontend/views/layouts/purple/partials/before_header.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<div class="cureency">
+    <div class="head_currency">
+        <span class="h5">Валюта курси:</span> <span class="bank">Ўз.Р.Марказий банки</span>
+    </div>
+    <div class="info">
+        <div class="foot_currency">
+
+            <div class="usd">
+                <div class="block"><span>USD</span> <br> <span>Lorem</span></div>
+                <div class="block"><span class="micro">АҚШ доллари</span> <span class="micro">lorem</span></div>
+            </div>
+
+            <div class="euro">
+                <div class="block"><span>EUR</span> <br> <span>Lorem</span></div>
+                <div class="block"><span class="micro">EВРО</span><span class="micro">lorem</span></div>
+            </div>
+
+            <div class="rub">
+                <div class="block"><span>RUB</span> <br> <span>Lorem</span></div>
+                <div class="block"><span class="micro">Россия рубли</span><span class="micro">lorem</span></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="running_info">
+    <marquee behavior="scroll" direction="left">Sayt test rejimida ishlayapti!!!</marquee>
+</div>
--- frontend/views/layouts/purple/site.php
+++ frontend/views/layouts/purple/site.php
@@ -29,34 +29,7 @@ $this->beginContent('@app/views/layouts/purple/main.php');
 
 <div class="main_body">
 
-    <div class="cureency">
-        <div class="head_currency">
-            <span class="h5">Валюта курси:</span> <span class="bank">Ўз.Р.Марказий банки</span>
-        </div>
-        <div class="info">
-            <div class="foot_currency">
-
-                <div class="usd">
-                    <div class="block"><span>USD</span> <br> <span>Lorem</span></div>
-                    <div class="block"><span class="micro">АҚШ доллари</span> <span class="micro">lorem</span></div>
-                </div>
-
-                <div class="euro">
-                    <div class="block"><span>EUR</span> <br> <span>Lorem</span></div>
-                    <div class="block"><span class="micro">EВРО</span><span class="micro">lorem</span></div>
-                </div>
-
-                <div class="rub">
-                    <div class="block"><span>RUB</span> <br> <span>Lorem</span></div>
-                    <div class="block"><span class="micro">Россия рубли</span><span class="micro">lorem</span></div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="running_info">
-        <marquee behavior="scroll" direction="left">Бегущая строка</marquee>
-    </div>
+    <?= $this->renderFile('@frontend/views/layouts/purple/partials/before_header.php') ?>
 
     <?= $this->renderFile('@frontend/views/layouts/purple/partials/header.php') ?>
 
